0061c0cc31d1a8ffeab0f9650639a9617835f756
lauuramarttin <martinyl@mail.gvsu.edu>
2024-11-07 23:13:00 -0500

    Update code.c

--- src/code.c
+++ src/code.c
@@ -435,9 +435,63 @@ void addNewPassengerTrip(list<Train> &Trains, map<string,Passenger> &Passengers)
 	}
 }
 
+// menu function
+bool menu(list<Train> &Trains, map<string, Passenger> &Passengers){
+	cout<<"Wellcome to the main menu, what do you want to do?"<<endl;
+	int option;
+	//display menu
+	cout<<"1. Add new passenger’s trip to a train"<<endl;
+	cout<<"2. Remove a passenger’s trip from a train"<<endl;
+	cout<<"3. Show trips of a passenger"<<endl;
+	cout<<"4. Show list of passengers in a specific train"<<endl;		
+	cout<<"5. Show an alphabetically ordered list of passengers in all the trains"<<endl;
+	//see when to finish the program
+	cout<<"6. End program"<<endl;
+	bool follow=1;
+	cout<<"Choose an option: ";
+	cin>>option;
+	if(option==6){
+		follow=0;
+	}
+	// stablish what to do for each option
+	switch (option){
+			case 1:
+				cout<<"Add new passengers trip to a train"<<endl;
+				addNewPassengerTrip(Trains,Passengers);
+				break;
+			case 2:
+				cout<<"Remove a passenger’s trip from a train"<<endl;
+				removePassengerTrip(Trains,Passengers);
+				break;
+			case 3:
+				cout<<"Show trips of a passenger"<<endl;
+				showTripsOfPassenger(Passengers);
+				break;
+			case 4:
+				cout<<"Show list of passengers in a specific train"<<endl;
+				showListOfPassengers(Trains, Passengers);
+				break;
+			case 5:
+				cout<<"Show an alphabetically ordered list of passengers in all the trains"<<endl;
+				ordList=showOrderedListOfPassengers(Passengers);
+				break;
+			case 6:
+				cout<<"End program"<<endl;
+				endProgram(Trains, Passengers);
+				break;
+			default:
+				cout<<"you've entered the wrong number"<<endl;
+				
+	}
+	return follow;	
+}
+
 int main (){
 	list<Train> Trains;
 	map<string,Passenger> Passengers;
 	readinitialData(Trains,Passengers);
-	cout<<"Read"<<endl;
+	bool follow;
+	do{
+		follow=menu(Trains, Passengers);
+	} while(follow!=0);
 }
